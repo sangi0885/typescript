b233735aa758eec425d52b33bd8979c65b734484
sangi0885 <sangi0885@gmail.com>
2022-06-18 16:45:14 -0700

    inital commit

---
+++ app.ts
@@ -0,0 +1,18 @@
+console.log('hello from TS!!!');
+function add(n1:number, n2:number){
+  return n1 + n2;
+}
+
+const number1 = 5;
+const number2 = 2.7;
+
+//console.log(add(number1, number2));
+const person = {
+//   name: string,
+//   age: number
+// } = {
+  name: 'Sangita Pandey',
+  age: 30,
+  hobbies: ['Sports', 'Cooking']
+}
+console.log(person.name);
